c11bf90f9c1ce12aef5558103f3d794effabdff7
Kamil Adamczyk <adamczykkamil995@gmail.com>
2023-03-12 11:35:01 +0100

    Dodanie zadanka

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import './../styles/styles.css';
 
 import { initializeApp } from 'firebase/app';
-import { getStorage } from "firebase/storage";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 // Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
@@ -17,14 +17,15 @@ const firebaseConfig = {
   appId: "1:584729761029:web:2e1c8c7d9bdf12b21bd36c"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 
-const url ="https://firebasestorage.googleapis.com/v0/b/sda-firebase-kamil.appspot.com/o/1643667975347.jpg?alt=media&token=d25c51ee-ae76-48a3-aabf-31237e6511c4";
+// const url ="https://firebasestorage.googleapis.com/v0/b/sda-firebase-kamil.appspot.com/o/1643667975347.jpg?alt=media&token=d25c51ee-ae76-48a3-aabf-31237e6511c4";
 
-const img = document.createElement("img");
-img.src = url;
-document.body.appendChild(img);
+// const img = document.createElement("img");
+// img.src = url;
+// document.body.appendChild(img);
 
 // function mojaFunkcja(callback) {
 //   callback();
@@ -41,6 +42,21 @@ document.body.appendChild(img);
 // .then((pupa123) => pupa123.json())
 // .then((data) => console.log(data));
 
-fetch("https://reqres.in/api/users")
-.then((daneeZPromisa) => daneeZPromisa.json())
-.then((daneZJson) => console.log(daneZJson.data));
+// fetch("https://reqres.in/api/users")
+// .then((daneeZPromisa) => daneeZPromisa.json())
+// .then((daneZJson) => console.log(daneZJson.data));
+
+// 1. Dodajemy input do HTML - typ file
+// 2. Dodajemy przycisk do HTML
+// 3. Do przycisku dodajemy obsługę kliknięcia
+// 4. Jako call back wywołujemy linijki z prezentacji 
+
+document.getElementById("myBtn").addEventListener("click", ()=> {
+const file = document.getElementById("myFile").files[0];
+const imageRef = ref(storage, "imageNew.jpg");
+
+uploadBytes(imageRef, file).then(() => {
+    console.log("Sukces!");
+  })
+})
+
